6ce40c421e040d5c756ae0f3378bf07c18403b26
tetsuka <g231o021@s.iwate-pu.ac.jp>
2017-04-16 10:17:42 +0900

    メッセージ表示領域のheightを再計算

--- js/thread.js
+++ js/thread.js
@@ -1,4 +1,4 @@
-$('#messages').height($(document).height() - ($('#thread_name').outerHeight(true) + $('#message-form').outerHeight(true) + 30));
+$('#messages').height(window.innerHeight - ($('#thread_name').outerHeight(true) + $('#message-form').outerHeight(true) + 30));
 
 function edit_message() {
   $('form').attr('action', '/board/edit_message.php');
